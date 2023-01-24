b0aa3be1827a728ec3916775a32ead6cc1fff4db
YunSef <sebblonski77@gmail.com>
2023-01-24 16:50:02 +0100

    solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'outputChallenge';
+  onomatopiaList : string[] = [];
+
+  onReceiveNewOnomatopia(event:string):void{
+
+    this.onomatopiaList.push(event)
+
+  }
 }
